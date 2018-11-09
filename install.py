fb9da7c454504abcf73d616a6805b5ad1769ac7a
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2018-11-09 17:44:01 +1100

    Update install.py

--- install.py
+++ install.py
@@ -4,8 +4,6 @@ import os
 def install():
 	print("Installing the required modules..\n")
 	os.system('pip install -r requirements.txt')
-	os.system("apt-get install tor")
-	os.system("apt-get install privoxy")
 	if (os.path.exists('reports') == False):	
 		print("Setting up some folders..\n")
 		os.system('mkdir reports')
