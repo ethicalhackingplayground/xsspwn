22147efa5202803f575a9e5677a9f06b7e782689
Krypt0mux <33673319+ethicalhackingplayground@users.noreply.github.com>
2018-11-09 17:53:54 +1100

    Update xsspwn.py

--- xsspwn.py
+++ xsspwn.py
@@ -39,7 +39,7 @@ def banner ():
  ▄███     ███▄     ▄█    ███    ▄█    ███   ███        ███ ▄█▄ ███ ███   ███ 
 ████       ███▄  ▄████████▀   ▄████████▀   ▄████▀       ▀███▀███▀   ▀█   █▀  
                                                                              
-		     XSS EXPLOIT AUTMOMATOR
+		     XSS EXPLOIT SCANNER
 			CODEBY:Krypt0Mux                                                      
 """
 	print (fg.red(msg))
